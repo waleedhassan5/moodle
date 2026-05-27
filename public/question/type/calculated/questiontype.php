4771c2dd9a5cb9e8a0fe0167df061f7205948ba6
Waleed Hassan <waleed.hassan@catalyst-eu.net>
2026-05-27 15:23:30 +0100

    fixup! MDL-83931 qtype_calculated: handle missing datasets

--- public/question/type/calculated/questiontype.php
+++ public/question/type/calculated/questiontype.php
@@ -402,9 +402,7 @@ class qtype_calculated extends question_type {
     public function restrict_status_if_setup_required(\MoodleQuickForm $mform, \stdClass $question): void {
         $qtype = question_bank::get_qtype($question->qtype);
 
-        $setuprequired = empty($question->id) || $qtype->question_requires_setup((int) $question->id);
-
-        if (!$setuprequired) {
+        if ($question->id && !$qtype->question_requires_setup((int) $question->id)) {
             return;
         }
 
